17ac3a2bc1f65c59e0a688e05bf07f4382529cf2
Amancio <amancio.jluis@outlook.com>
2025-06-25 06:46:50 -0300

    JLA - Testando tipos de blocos (if) , encapsulamento de funções , escopo de variáveis

--- src/index.js
+++ src/index.js
@@ -68,11 +68,16 @@ async function getRandomBlock() {
     return result;
 }
 
+async function logRollResult(characterName, block, diceResult, attribute) {
+    console.log(`${characterName} 🎲 rolou um dado de ${block} ${diceResult}`)
+
+}
+
 
 async function playRaceEngine(character1, character2) {
     console.log('Inicio da execução da corrida')
     // sortear bloco
-    for(let round=1; round <=5; round++){              
+    for (let round = 1; round <= 5; round++) {
         console.log(`🏁🏍️  - Rodada: ${round}`);
         let block = await getRandomBlock();
         console.log(`Bloco: ${block}`)
@@ -85,17 +90,50 @@ async function playRaceEngine(character1, character2) {
         let totalTestSkill1 = 0;
         let totalTestSkill2 = 0;
 
-        if(block === "RETA"){
+        if (block === "RETA") {
             totalTestSkill1 = diceResult1 + character1.velocidade
             totalTestSkill2 = diceResult2 + character2.velocidade
+
+            await logRollResult(
+                character1.nome,
+                "velocidade",
+                diceResult1,
+                character1.velocidade
+            );
+
+            await logRollResult(
+                character2.nome,
+                "velocidade",
+                diceResult2,
+                character2.velocidade
+            );
+
         }
-        if(block === "CURVA"){
+
+        if (block === "CURVA") {
             totalTestSkill1 = diceResult1 + character1.manobrabilidade
             totalTestSkill2 = diceResult2 + character2.manobrabilidade
+
+              await logRollResult(
+                character1.nome,
+                "manobrabilidade",
+                diceResult1,
+                character1.manobrabilidade
+            );
+
+            await logRollResult(
+                character2.nome,
+                "manobrabilidade",
+                diceResult2,
+                character2.manobrabilidade
+            );
+
         }
-        if(block === "CONFRONTO"){
-            let powerResult1 = diceResult1 + character1.poder 
-            let powerResult2 = diceResult2 + character2.poder 
+
+        if (block === "CONFRONTO") {
+            let powerResult1 = diceResult1 + character1.poder
+            let powerResult2 = diceResult2 + character2.poder
+
         }
 
     }
